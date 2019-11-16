86ad090e934db85d9bdd9b9650b3c38bd24f011d
Can Xia <canx@andrew.cmu.edu>
2019-11-16 00:07:13 -0800

    Add: create configs

--- handout/server.py
+++ handout/server.py
@@ -5,6 +5,9 @@ import json
 
 app = Flask(__name__)
 
+root_dir = osp.dirname(osp.realpath(__file__))
+config_dir = osp.join(root_dir, 'configs')
+
 
 @app.route('/config', methods=['POST'])
 def create_config_file():
@@ -12,12 +15,12 @@ def create_config_file():
     config_path = osp.join('configs', '{}.cfg'.format(config_obj['name']))
     if not osp.exists(config_path):
         with open(config_path, 'w') as fp:
-            fp.write(request.data)
+            fp.write(request.data.decode('utf-8'))
     return '', 200
 
 
 if __name__ == '__main__':
-    os.chdir(osp.dirname(osp.realpath(__file__)))
-    if not osp.exists('configs'):
-        os.makedirs('configs')
+    os.chdir(root_dir)
+    if not osp.exists(config_dir):
+        os.makedirs(config_dir)
     app.run(host='localhost', port=8080)
